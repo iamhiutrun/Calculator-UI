d92014a40f167183b4d7ed419fa26f5d071b44f0
Lương Trung Hiếu <hieu.luongtrung@ddsolution.vn>
2021-12-13 23:04:17 +0700

    Implement Logic Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,6 +31,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var userInput = '123';
+  var answer = '';
+
+  final List<String> _buttons = [
+    'c',
+    '+/-',
+    '%',
+    'DEL',
+    '7',
+    '8',
+    '9',
+    '/',
+    '4',
+    '5',
+    '6',
+    'x',
+    '1',
+    '2',
+    '3',
+    '-',
+    '0',
+    '.',
+    '=',
+    '+'
+  ];
+
+  bool isOperator(String x) {
+    if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+      return true;
+    }
+    return false;
+  }
+
+  void onEqualButtonPressed() {
+    String calculation = userInput.replaceAll('x', '*');
+    Parser p = Parser();
+    Expression exp = p.parse(calculation);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,93 +85,115 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Spacer(),
+            // const Spacer(),
             Expanded(
-              flex: 3,
+              flex: 2,
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Container(
+                  width: double.infinity,
                   decoration: BoxDecoration(
                       border: Border.all(
                         color: Colors.white,
                       ),
                       borderRadius:
                           const BorderRadius.all(Radius.circular(10))),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        alignment: Alignment.centerRight,
+                        child: Text(userInput,
+                            textAlign: TextAlign.end,
+                            style: const TextStyle(
+                                fontSize: 18, color: Colors.white)),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(15),
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          answer,
+                          style: const TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
             Expanded(
               flex: 5,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Button(text: 'C', callback: (String value) {}),
-                      Button(text: '\$', callback: (String value) {}),
-                      Button(text: '%', callback: (String value) {}),
-                      Button(
-                          text: '/',
-                          isCircle: true,
-                          callback: (String value) {}),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Button(text: '7', callback: (String value) {}),
-                      Button(text: '8', callback: (String value) {}),
-                      Button(text: '9', callback: (String value) {}),
-                      Button(
-                          text: '-',
-                          isCircle: true,
-                          callback: (String value) {}),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Button(text: '4', callback: (String value) {}),
-                      Button(text: '5', callback: (String value) {}),
-                      Button(text: '6', callback: (String value) {}),
-                      Button(
-                          text: '+',
-                          isCircle: true,
-                          callback: (String value) {}),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Button(text: '1', callback: (String value) {}),
-                      Button(text: '2', callback: (String value) {}),
-                      Button(text: '3', callback: (String value) {}),
-                      Button(
-                          text: 'x',
-                          isLarge: false,
-                          isCircle: true,
-                          callback: (String value) {}),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Button(
-                          text: '0',
-                          isLarge: true,
-                          callback: (String value) {}),
-                      Button(text: '.', callback: (String value) {}),
-                      Button(
-                          text: '=',
-                          isCircle: true,
-                          callback: (String value) {}),
-                    ],
-                  ),
-                  const Spacer(),
-                ],
+              child: GridView.builder(
+                dragStartBehavior: DragStartBehavior.start,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                itemCount: _buttons.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4),
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    // C button
+                    return Button(
+                      text: _buttons[0],
+                      callback: (value) {
+                        setState(() {
+                          userInput = '';
+                          answer = '';
+                        });
+                      },
+                    );
+                  } else if (index == 1) {
+                    // negate userInput
+                    return Button(
+                      text: _buttons[1],
+                      callback: (value) {
+                        setState(() {
+                          userInput = '-(' + userInput + ')';
+                        });
+                      },
+                    );
+                  } else if (index == 3) {
+                    return Button(
+                      text: _buttons[3],
+                      callback: (value) {
+                        setState(() {
+                          userInput =
+                              userInput.substring(0, userInput.length - 1);
+                        });
+                      },
+                    );
+                  } else if (index == 18) {
+                    // = button
+                    return Button(
+                      color: Colors.green,
+                      text: _buttons[18],
+                      callback: (value) {
+                        setState(() {
+                          onEqualButtonPressed();
+                        });
+                      },
+                    );
+                  } else {
+                    // other buttons
+                    return Button(
+                      text: _buttons[index],
+                      color: isOperator(_buttons[index])
+                          ? Colors.red
+                          : Colors.black,
+                      callback: (value) {
+                        setState(() {
+                          userInput += value;
+                        });
+                      },
+                    );
+                  }
+                },
               ),
             ),
+            // const Spacer(),
           ],
         ),
       ),
@@ -139,15 +205,15 @@ class Button extends StatelessWidget {
   final String text;
   final bool isLarge;
   final bool isCircle;
-  final int textColor;
+  final Color? color;
   final Function callback;
   const Button(
       {required this.text,
       this.isLarge = false,
       this.isCircle = false,
-      this.textColor = 0xFFFFFFFF,
       required this.callback,
-      Key? key})
+      Key? key,
+      this.color})
       : super(key: key);
 
   @override
@@ -156,7 +222,7 @@ class Button extends StatelessWidget {
       margin: const EdgeInsets.only(top: 5, right: 10),
       decoration: BoxDecoration(
           border: Border.all(
-            color: Colors.black,
+            color: color ?? Colors.black,
           ),
           borderRadius: BorderRadius.all(Radius.circular(isCircle ? 30 : 10))),
       width: isLarge ? 140 : 65,
